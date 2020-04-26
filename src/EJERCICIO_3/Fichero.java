ac389c676cb380a40f65d0b38372a4c098878b85
Dani <danirm1113rgmail.com>
2020-04-26 18:02:20 +0200

    Creación del método listarAlumno en el ejercicio 3

--- src/EJERCICIO_3/Fichero.java
+++ src/EJERCICIO_3/Fichero.java
@@ -1,8 +1,10 @@
 package EJERCICIO_3;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class Fichero {
@@ -21,5 +23,27 @@ public class Fichero {
 		} catch (FileNotFoundException e) {
 			System.out.println("No puedo crear el fichero");
 		}
+	}
+	
+	public static Alumno listarAlumno(String fichero) {
+		Alumno a = null;
+		try {
+			FileInputStream ficheroEntrada = new FileInputStream (fichero);
+			try {
+				ObjectInputStream datosFicheroEntrada = new ObjectInputStream (ficheroEntrada);
+				try {
+					a = (Alumno)datosFicheroEntrada.readObject();
+				datosFicheroEntrada.close();
+				ficheroEntrada.close();
+				} catch (ClassNotFoundException e) {
+					System.out.println("No se ha podido encontrar la clase producto");
+				}
+			} catch (IOException e) {
+				System.out.println("No se ha podido recuperar el objeto");
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Fichero no encontrado");
+		}
+		return a;
 	}
 }
