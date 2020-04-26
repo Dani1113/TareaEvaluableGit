af7fc16f894554de8ed852fa4c80dad28ad08e94
Dani <danirm1113rgmail.com>
2020-04-26 17:58:28 +0200

    Creación del método añadirAlumno en la clase del fichero serializable

--- src/EJERCICIO_3/Fichero.java
+++ src/EJERCICIO_3/Fichero.java
@@ -1,5 +1,25 @@
 package EJERCICIO_3;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class Fichero {
 
+	public static void añadirAlumno(String fichero, Alumno a1){
+		try {
+			FileOutputStream ficheroSalida = new FileOutputStream(fichero);
+			try {
+				ObjectOutputStream datosFicheroSalida = new ObjectOutputStream(ficheroSalida);
+				datosFicheroSalida.writeObject(a1);
+				datosFicheroSalida.close();
+				ficheroSalida.close();
+			} catch (IOException e) {
+				System.out.println("No puedo escribir el objeto en el fichero");
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("No puedo crear el fichero");
+		}
+	}
 }
