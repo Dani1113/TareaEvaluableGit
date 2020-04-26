771eec2088c1809fb68480e84d788dd3f3411ead
Dani <danirm1113rgmail.com>
2020-04-26 17:41:39 +0200

    Borrado de el ejercicio 1 y 2 de la rama Nueva_rama

--- EJERCICIO_1/PruebaFichero.java
+++
@@ -1,9 +0,0 @@
-package EJERCICIO_1;
-
-public class PruebaFichero {
-
-	public static void main(String[] args) {
-		Fichero.encriptarFichero("D:\\Dani\\Documentos\\CFGS DAM\\PROGRAMACIÓN\\TEMA 7 TAREA 1 - TEMA 4 ED PRÁCTICA 1\\FICHEROS\\FICHERO.txt", "D:\\Dani\\Documentos\\CFGS DAM\\PROGRAMACIÓN\\TEMA 7 TAREA 1 - TEMA 4 ED PRÁCTICA 1\\FICHEROS\\FICHERO_CIFRADO.txt");		
-		Fichero.desencriptarFichero("D:\\Dani\\Documentos\\CFGS DAM\\PROGRAMACIÓN\\TEMA 7 TAREA 1 - TEMA 4 ED PRÁCTICA 1\\FICHEROS\\FICHERO_CIFRADO.txt", "D:\\Dani\\Documentos\\CFGS DAM\\PROGRAMACIÓN\\TEMA 7 TAREA 1 - TEMA 4 ED PRÁCTICA 1\\FICHEROS\\FICHERO_DESCIFRADO.txt" );
-	}
-}
